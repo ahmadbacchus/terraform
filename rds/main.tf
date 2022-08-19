6168caf0b135b19526606b90bb99edfa4f187173
ahmadbacchus <abacchus@outlook.com>
2022-08-18 21:19:32 -0400

    nnn

---
+++ rds/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  db_name              = "ahmadtestdb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
--- vm/main.tf
+++ vm/main.tf
@@ -16,8 +16,8 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name = "ahmad"
-  vpc_security_group_ids = ["sg-0226c0186ef0176b9"]
+  key_name = var.key-name
+  vpc_security_group_ids = var.vpc_security_group_ids
   user_data = file("install.sh")
   tags = {
     Name = var.tags-name
